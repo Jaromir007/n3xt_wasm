b05e78031f9d529bc9dcf878abef84d226ddb6b4
Jaromir007 <jaromir.obitko@student.gyarab.cz>
2025-02-26 13:04:16 +0100

    not optimized, but works!

--- slicer/Main.cpp
+++ slicer/Main.cpp
@@ -1,8 +1,12 @@
 #include "ParseSTL.hpp"
 #include "Slicer.hpp"
 #include <iostream>
+#include <chrono>
+#include <sstream>
+#include <fstream>
 
 using namespace std; 
+using namespace std::chrono; 
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -13,17 +17,45 @@ int main(int argc, char* argv[]) {
     string filename = argv[1];
     float layerHeight = 0.2f;
 
+    auto start = high_resolution_clock::now(); 
     auto triangles = parseSTL(filename);
     if (triangles.empty()) return 1;
 
     auto layers = slice(triangles, layerHeight);
-    
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+
+    cout << "Done" << "\n" << "[TIMING] Slicing took: " << duration.count() << " ms" << "\n"; 
+
+    stringstream json_out;
+    json_out << "[\n";
+
     for (size_t i = 0; i < layers.size(); ++i) {
-        cout << "Layer " << i << " (Z = " << (i * layerHeight) << ")\n";
-        for (const auto& p : layers[i]) {
-            cout << "(" << p.x << ", " << p.y << ", " << p.z << ")\n";
+        if (i > 0) json_out << ",\n";
+        json_out << "  [\n";
+
+        for (size_t j = 0; j < layers[i].size(); ++j) {
+            const auto& p = layers[i][j];
+            if (j > 0) json_out << ",\n";
+            json_out << "    [" << p.x << ", " << p.y << "]";
         }
+
+        json_out << "\n  ]";
     }
+
+    json_out << "\n]";
+
+    ofstream out_file("../../out.json");
+    if (!out_file) {
+        cerr << "Error opening file for writing." << endl;
+        return 1;
+    }
+
+    out_file << json_out.str();
+    out_file.close();
+
+    cout << "JSON saved to out.json" << endl;
+
     
     return 0;
 }
--- slicer/Slicer.cpp
+++ slicer/Slicer.cpp
@@ -12,18 +12,17 @@ vector<vector<Vec3>> slice(const vector<Triangle>& triangles, float layerHeight)
         maxZ = max({maxZ, tri.v1.z, tri.v2.z, tri.v3.z});
     }
 
-    for (float zi = minZ; zi <= maxZ; zi += layerHeight) {
+    for (float zp = minZ; zp <= maxZ; zp += layerHeight) {
         vector<Vec3> layer;
         for (const auto& tri : triangles) {
             Vec3 edges[3][2] = {{tri.v1, tri.v2}, {tri.v2, tri.v3}, {tri.v3, tri.v1}};
             for (auto& edge : edges) {
                 Vec3 p1 = edge[0], p2 = edge[1];
-                if ((p1.z < zi && p2.z > zi) || (p1.z > zi && p2.z < zi)) {
-                    float t = (zi - p1.z) / (p2.z - p1.z);
+                if ((p1.z < zp && p2.z > zp) || (p1.z > zp && p2.z < zp)) {
+                    float t = (zp - p1.z) / (p2.z - p1.z);
                     layer.push_back({
                         p1.x + t * (p2.x - p1.x),
                         p1.y + t * (p2.y - p1.y),
-                        zi
                     });
                 }
             }
